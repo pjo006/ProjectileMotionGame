b78c652c4598a28085e8a2700a85ee2339190539
PJ Onusconich <pjo006@bucknell.edu>
2017-10-02 16:19:13 -0400

    Added power bar

---
+++ Assets/Scripts/PowerBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerBar : MonoBehaviour {
+
+	public Slider powerBar;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		powerBar.value = Input.mousePosition.x / 1400f;
+	}
+}
