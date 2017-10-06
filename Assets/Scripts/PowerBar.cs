f1cbb9885b479b2bb38fa85f8567c934783d7bc9
PJ Onusconich <pjo006@bucknell.edu>
2017-10-06 15:42:59 -0400

    power bar added again

--- Assets/Scripts/PowerBar.cs
+++ Assets/Scripts/PowerBar.cs
@@ -6,14 +6,22 @@ using UnityEngine.UI;
 public class PowerBar : MonoBehaviour {
 
 	public Slider powerBar;
+	public Image Fill;
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		powerBar.value = Input.mousePosition.x / 1400f;
+		if (powerBar.value < 0.33f) {
+			Fill.color = Color.green;	
+		} else if (powerBar.value < 0.66f) {
+			Fill.color = Color.yellow;
+		} else if (powerBar.value < 0.9f) {
+			Fill.color = Color.red;
+		}
 	}
 }
