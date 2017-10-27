9a32f5d4f8965095ec5f0857de6a49c1fe3b7ccf
Dale Hartman <dah054@bucknell.edu>
2017-10-27 14:28:42 -0400

    Added Hitsounds for collisions between ball and other game objects

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,15 +5,34 @@ using UnityEngine;
 public class Ball : MonoBehaviour {
 
 	public static int num = 0;
+	public AudioClip greenHitSound;
+	public AudioClip redHitSound;
+	public AudioClip platformHitSound;
+
+	private AudioSource source;
 
 	// Use this for initialization
 	void Start () {
 		num++;
+		source = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.name == "Green(Clone)") {
+			source.PlayOneShot(greenHitSound, 1.0f);
+		}
+		else if (collision.gameObject.name == "Red(Clone)") {
+			source.PlayOneShot(redHitSound, 1.0f);
+		}
+		else if (collision.gameObject.name == "Platform(Clone)") {
+			source.PlayOneShot(platformHitSound, 1.0f);
+		}
+
 	}
 
 	void OnDestroy () {
